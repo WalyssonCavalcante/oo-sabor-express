6d7f6f5ea2d9aa99da344f903232fb62e5f70aa9
Wallysson Cavalcante <wallysson.cavalcante@gmail.com>
2025-10-08 11:03:20 -0300

    Property

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,14 +1,25 @@
 class Restaurant:
+    restaurants = []
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.active = False
+        self._active = False
+        Restaurant.restaurants.append(self)
     
     def __str__(self):
         return f'{self.name} | {self.category}'
+    
+    def list_restaurants():
+        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}')
+
+    @property
+    def active(self):
+        return 'True ✅' if self._active else 'False ❌'
+
 
 restaurant_hamburguer = Restaurant('Hamburguer', 'Gourmet')
 restaurant_pizza = Restaurant('Pizza', 'Italiana')
 
-print (restaurant_hamburguer)
-print (restaurant_pizza)
+Restaurant.list_restaurants()
